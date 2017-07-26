513bdd8831340300f374162e0ca6bd6aa98a7422
Victor Hugo Teles Costa <victor.hugooooo@gmail.com>
2017-07-26 15:07:53 -0300

    Fixing relative paths

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var DateRangePicker = require('./lib/components/DateRangePicker').default;
-var DateRangePickerInput = require('./lib/components/DateRangePickerInput').default;
-var DateRangePickerInputController = require('./lib/components/DateRangePickerInputController').default;
-var SingleDatePicker = require('./lib/components/SingleDatePicker').default;
-var SingleDatePickerInput = require('./lib/components/SingleDatePickerInput').default;
-var DayPicker = require('./lib/components/DayPicker').default;
-var DayPickerRangeController = require('./lib/components/DayPickerRangeController').default;
-var CalendarMonthGrid = require('./lib/components/CalendarMonthGrid').default;
-var CalendarMonth = require('./lib/components/CalendarMonth').default;
-var CalendarDay = require('./lib/components/CalendarDay').default;
+var DateRangePicker = require('./src/components/DateRangePicker').default;
+var DateRangePickerInput = require('./src/components/DateRangePickerInput').default;
+var DateRangePickerInputController = require('./src/components/DateRangePickerInputController').default;
+var SingleDatePicker = require('./src/components/SingleDatePicker').default;
+var SingleDatePickerInput = require('./src/components/SingleDatePickerInput').default;
+var DayPicker = require('./src/components/DayPicker').default;
+var DayPickerRangeController = require('./src/components/DayPickerRangeController').default;
+var CalendarMonthGrid = require('./src/components/CalendarMonthGrid').default;
+var CalendarMonth = require('./src/components/CalendarMonth').default;
+var CalendarDay = require('./src/components/CalendarDay').default;
 
-var DateRangePickerShape = require('./lib/shapes/DateRangePickerShape').default;
-var SingleDatePickerShape = require('./lib/shapes/SingleDatePickerShape').default;
+var DateRangePickerShape = require('./src/shapes/DateRangePickerShape').default;
+var SingleDatePickerShape = require('./src/shapes/SingleDatePickerShape').default;
 
-var isInclusivelyAfterDay = require('./lib/utils/isInclusivelyAfterDay').default;
-var isInclusivelyBeforeDay = require('./lib/utils/isInclusivelyBeforeDay').default;
-var isNextDay = require('./lib/utils/isNextDay').default;
-var isSameDay = require('./lib/utils/isSameDay').default;
+var isInclusivelyAfterDay = require('./src/utils/isInclusivelyAfterDay').default;
+var isInclusivelyBeforeDay = require('./src/utils/isInclusivelyBeforeDay').default;
+var isNextDay = require('./src/utils/isNextDay').default;
+var isSameDay = require('./src/utils/isSameDay').default;
 
-var toISODateString = require('./lib/utils/toISODateString').default;
-var toLocalizedDateString = require('./lib/utils/toLocalizedDateString').default;
-var toMomentObject = require('./lib/utils/toMomentObject').default;
+var toISODateString = require('./src/utils/toISODateString').default;
+var toLocalizedDateString = require('./src/utils/toLocalizedDateString').default;
+var toMomentObject = require('./src/utils/toMomentObject').default;
 
 
 module.exports = {
